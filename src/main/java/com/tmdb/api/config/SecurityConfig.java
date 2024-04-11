0cf40b99aed7bebbdd3f69f47304f241cfe0eec2
Rocha <cl.rch09@icloud.com>
2024-04-11 00:43:39 -0300

    refactor: add config parameters to SecurityConfig class

--- src/main/java/com/tmdb/api/config/SecurityConfig.java
+++ src/main/java/com/tmdb/api/config/SecurityConfig.java
@@ -1,26 +1,64 @@
 package com.tmdb.api.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import jakarta.servlet.DispatcherType;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Value("${jwt.public.key}")
+  private RSAPublicKey publicKey;
+
+  @Value("${jwt.private.key}")
+  private RSAPrivateKey privateKey;
+
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
-        authorize -> authorize.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll().anyRequest()
-            .permitAll()).csrf(csrf -> csrf.disable()).sessionManagement(
-        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            authorize -> authorize.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+                .anyRequest().authenticated()).csrf(csrf -> csrf.disable())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
 
+  @Bean
+  JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+  }
 
+  @Bean
+  JwtEncoder jwtEncoder() {
+    var jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+    var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
+  }
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
