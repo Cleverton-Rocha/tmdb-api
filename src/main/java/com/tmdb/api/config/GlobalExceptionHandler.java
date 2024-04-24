fd320192b374bf65416356ced3de33b2478c0134
Rocha <cl.rch09@icloud.com>
2024-04-24 00:56:05 -0300

    feat: add BadCredentialsException to GlobalExceptionHandler

--- src/main/java/com/tmdb/api/config/GlobalExceptionHandler.java
+++ src/main/java/com/tmdb/api/config/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.tmdb.api.config;
 import com.tmdb.api.exception.NotFoundException;
 import java.util.Objects;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,13 +15,18 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
-    return ResponseEntity.status(404).body(ex.getMessage());
+    return ResponseEntity.status(400).body(ex.getMessage());
   }
 
   @ExceptionHandler(FeignException.NotFound.class)
   public ResponseEntity<String> handleFeignNotFound(
       FeignException.NotFound ex) {
     return ResponseEntity.status(404).body("Movie not found.");
+  }
+
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
+    return ResponseEntity.status(401).body(ex.getMessage());
   }
 
   @ExceptionHandler(FeignException.Unauthorized.class)
